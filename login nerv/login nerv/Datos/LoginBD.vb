2266ddc25be590bc42cc6d2c85761011fbbfc801
YojanR <ydrv.148@gmail.com>
2018-04-30 18:02:31 -0600

    cambios a la estructura

---
+++ login nerv/login nerv/Datos/LoginBD.vb
@@ -0,0 +1,3 @@
+﻿Public Class LoginBD
+
+End Class
---
+++ login nerv/login nerv/Presentacion/Login.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+Button1: If TextBox1.Text = "Uriel" And TextBox2.Text = "1234" Then
+            MsgBox("Bienvenido a NERV")
+            MenuPrincipal.Show()
+            Me.Hide()
+        Else
+            MsgBox("La contraseña es incorrecta", MsgBoxStyle.Exclamation, "Error")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
